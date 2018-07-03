ad8a1c5ef2870965a8b6461d703f0f9dfcf32efc
Gauravsunil <gauravsunil1489@gmail.com>
2018-07-03 12:21:06 +0530

    my projewct

---
+++ first.py
@@ -0,0 +1,15 @@
+print("gaurav")
+
+a=10
+b=20
+print(a+b)
+
+print("MY AGE IS "+str(a)+" YEARS")
+
+print("My age is {0} years".format(a))
+
+print("""January={2}
+February={0}
+march={2}
+April={1}""".format(28,30,31))
+print("My age is %d %s" %(a ,"years"))
